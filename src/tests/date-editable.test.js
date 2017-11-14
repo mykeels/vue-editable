1446408ed92113da916db18fd06d42729cf636f4
Ikechi Michael <mykehell123@gmail.com>
2017-11-14 21:20:52 +0100

    test: setup assertions for date and select editable

---
+++ src/tests/date-editable.test.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import { CreateDateEditable } from './helpers/create-comp'
+
+describe('DateEditable', () => {
+    it('should mount', () => {
+        
+    })
+})
---
+++ src/tests/select-editable.test.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import { CreateSelectEditable } from './helpers/create-comp'
+
+describe('SelectEditable', () => {
+    it('should mount', () => {
+        
+    })
+})
